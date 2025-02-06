67ff546d46b066e3c15b4de4d017eecfb98a4eb7
chinmay21 <dhaundiyalchinmay@gmail.com>
2025-02-06 18:54:15 +0530

    Add styling

--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -1,5 +1,7 @@
 import "./Home.css";
 import { ChangeEvent, useState } from "react";
+import bgTop from '../images/bg-top.svg';
+import bgBottom from '../images/bg-bottom.svg';
 const Home = () => {
     
     const [basicPrice, setBasicPrice] = useState(19.99);
@@ -22,7 +24,7 @@ const Home = () => {
 
     return(
         //Parent Container
-        <div className="w-[100vw] h-[100vh] flex flex-col items-center pr-[300px] py-2"> 
+        <div className="w-[100vw] h-[100vh] flex flex-col items-center py-2 relative"> 
             <p className="font-bold text-3xl text-[#6d708d] pb-5">Our Pricing</p>
                 {/* Toggle button */}
                 <div className="flex justify-center items-center gap-x-5">
@@ -34,9 +36,9 @@ const Home = () => {
                  <p className="font-bold">Monthly</p>         
                 </div> 
                 {/* Cards Parent Conatiner */}
-                <div className="flex mt-[50px]">
+                <div className="flex mt-[50px] relative z-20">
                     {/* Basic Card */}    
-                    <div className="w-[350px] h-[450px] bg-[#FFFFFF] rounded-xl mt-[25px]">
+                    <div className="w-[350px] text-center h-[450px] bg-[#FFFFFF] rounded-xl mt-[25px]">
                       <p className="text-[#B3B5C6] font-bold text-lg pt-8">Basic</p>
                       <p className="text-[70px] text-[#494C5F] font-bold pb-4"><span className="text-[40px] bottom-2 relative">$</span>{basicPrice}</p>
                       <div className="w-[85%] h-[1.5px] bg-[#b3B5C6] ml-[30px]"></div>
@@ -46,10 +48,10 @@ const Home = () => {
                       <div className="w-[85%] h-[1.5px] bg-[#b3B5C6] ml-[30px]"></div>
                       <p className="text-[#B3B5C6] font-bold py-3">Send up to 3 GB</p>
                       <div className="w-[85%] h-[1.5px] bg-[#b3B5C6] ml-[30px]"></div>
-                      <button>LEARN MORE</button>
+                      <button className="w-[300px] text-white font-bold ml-1 py-3 h-[40px] border border-[#b3B5C6] pb-8 rounded-md mt-[40px] bg-[#848AE6] btn">LEARN MORE</button>
                     </div>
                     {/* Professional Card */}
-                    <div className="w-[380px] h-[500px] bg-gradient-to-b from-[#9b90ff] to-[#8689E2] rounded-xl pt-[22px]">
+                    <div className="w-[380px] text-center h-[500px] bg-gradient-to-b from-[#9b90ff] to-[#8689E2] rounded-xl pt-[22px]">
                         <div>
                         <p className="text-[#FFFFFF] font-bold text-lg pt-8">Professional</p>
                         <p className="text-[70px] text-[#FFFFFF] font-bold pb-4"><span className="text-[40px] bottom-2 relative">$</span>{professionalPrice}</p>
@@ -60,11 +62,11 @@ const Home = () => {
                         <div className="w-[85%] h-[1.5px] bg-[#b3B5C6] ml-[30px]"></div>
                         <p  className="text-[#FFFFFF] font-bold py-3">Send up to 10 GB</p>
                         <div className="w-[85%] h-[1.5px] bg-[#b3B5C6] ml-[30px]"></div>
-                        <button>LEARN MORE</button>
+                        <button className="w-[320px] text-[#848AE6] font-bold ml-1 py-3 h-[40px] border border-[#FFFFFF] pb-8 rounded-md mt-[40px] bg-[#FFFFFF] transition-all delay-75 ease-in professional-btn">LEARN MORE</button>
                         </div>
                     </div>
                     {/* Master Card */}
-                    <div className="w-[350px] h-[450px] bg-[#FFFFFF] rounded-xl mt-[25px]">
+                    <div className="w-[350px] text-center h-[450px] bg-[#FFFFFF] rounded-xl mt-[25px]">
                         <div>
                         <p className="text-[#B3B5C6] font-bold text-lg pt-8">Master</p>
                         <p className="text-[70px] text-[#494C5F] font-bold pb-4"><span className="text-[40px] bottom-2 relative">$</span>{masterPrice}</p>
@@ -75,10 +77,12 @@ const Home = () => {
                         <div className="w-[85%] h-[1.5px] bg-[#b3B5C6] ml-[30px]"></div>
                         <p className="text-[#B3B5C6] font-bold py-3">Send up to 20 GB</p>
                         <div className="w-[85%] h-[1.5px] bg-[#b3B5C6] ml-[30px]"></div>
-                        <button>LEARN MORE</button>
+                        <button className="w-[300px] text-white font-bold ml-1 py-3 h-[40px] border border-[#b3B5C6] pb-8 rounded-md mt-[40px] bg-[#848AE6] btn">LEARN MORE</button>
                         </div>
                     </div>
                 </div>
+                <img className="absolute z-10 right-[-1px] top-[1px]" src={bgTop} loading="lazy" alt="top-svg"></img>
+                <img className="absolute z-10 bottom-[-1px] left-[-1px]" src={bgBottom} loading="lazy" alt="bg-svg"></img>
         </div>
     )
 }
